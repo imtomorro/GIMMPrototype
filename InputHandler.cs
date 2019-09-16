5f8e57b94900860d20809b84767c24539bc70c10
Rowan O'Neill <54593450+imtomorro@users.noreply.github.com>
2019-09-15 23:16:20 -0600

    Add files via upload

---
+++ InputHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputHandler : MonoBehaviour {
+
+    public InputField inputText;
+    public Text outputText;
+    public Button textButton;
+    public int avNum = 0;
+    public int avAlph = 0;
+	// Use this for initialization
+	void Start () {
+        outputText.text = "";
+    }
+    public void processText()
+    {
+        outputText.text = Utilities.ProcessText(inputText.text);
+        string[] nums = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        foreach (string num in nums)
+        {
+            if (inputText.text.Contains(num))
+            {
+                avNum = inputText.text.Length;
+                string avNumText = avNum.ToString();
+                outputText.text = avNumText;
+            }
+        }
+        string[] alpha = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        foreach (string alph in alpha)
+        {
+            if (inputText.text.Contains(alph))
+            {
+                inputText.text.Split(' ');
+                avAlph = inputText.text.Length;
+                string avAlphText = avAlph.ToString();
+                outputText.text = avAlphText;
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static string ProcessText(string textIn)
+    {
+
+        return textIn;
+        //return the word 'string' if the user enters a string
+        //return the word 'number' if the user enters a number (including 1.2 decimalish numbers called doubles, floats)
+    }
+}
